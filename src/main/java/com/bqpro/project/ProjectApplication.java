7be8055879e6f0a8268409f93e4f9d04885bcd70
pavelrivera85 <>
2023-09-07 14:50:20 +0000

    initial address peoples

--- src/main/java/com/bqpro/project/ProjectApplication.java
+++ src/main/java/com/bqpro/project/ProjectApplication.java
@@ -1,8 +1,10 @@
 package com.bqpro.project;
 
 import com.bqpro.project.Enums.ERole;
+import com.bqpro.project.Model.Address;
 import com.bqpro.project.Model.Person;
 import com.bqpro.project.Model.Role;
+import com.bqpro.project.Repository.AddressRepository;
 import com.bqpro.project.Repository.PersonRepository;
 import com.bqpro.project.Repository.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -23,17 +25,41 @@ public class ProjectApplication {
 
 
 	@Bean
-	CommandLineRunner commandLineRunner(PersonRepository personRepository, RoleRepository roleRepository) {
+	CommandLineRunner commandLineRunner(PersonRepository personRepository, RoleRepository roleRepository, AddressRepository addressRepository) {
 
 		return args -> {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-			personRepository.save(new Person("John", "Doe",  new Date(dateFormat.parse("1990-01-01").getTime()), "555-1234", null));
-			personRepository.save(new Person("Jane", "Smith",  new Date(dateFormat.parse("1985-06-15").getTime()), "555-5678", null));
-			personRepository.save(new Person("Michael", "Johnson",  new Date(dateFormat.parse("1992-12-31").getTime()), "555-9876", null));
+			Person p1=new Person("John", "Doe", new Date(dateFormat.parse("1990-01-01").getTime()), "555-1234", null);
+			Person p2=new Person("Jane", "Smith", new Date(dateFormat.parse("1985-06-15").getTime()), "555-5678", null);
+			Person p3=new Person("Michael", "Johnson", new Date(dateFormat.parse("1992-12-31").getTime()), "555-9876", null);
+
+			personRepository.save(p1);
+			personRepository.save(p2);
+			personRepository.save(p3);
 
 			roleRepository.save(new Role(ERole.valueOf("ROLE_ADMIN")));
 			roleRepository.save(new Role(ERole.valueOf("ROLE_USER")));
+
+			Address a1= new Address();
+			a1.setText("123 Main St");
+			Address a11= new Address();
+			a11.setText("456 Miami St");
+			a1.setPerson(p1);
+			a11.setPerson(p1);
+			
+			Address a2= new Address();
+			a2.setText("456 Elm St");
+			a2.setPerson(p2);
+
+			Address a3= new Address();
+			a3.setText("789 Oak St");
+			a3.setPerson(p3);
+
+			addressRepository.save(a1);
+			addressRepository.save(a11);
+			addressRepository.save(a2);
+			addressRepository.save(a3);
 
 		};
 
